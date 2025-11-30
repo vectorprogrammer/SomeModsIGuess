8a75d8c8aa3cb9d72f4f185a83175f1160fd83e5
Liu Yihao <154865817+vectorprogrammer@users.noreply.github.com>
2025-11-30 21:55:13 +0800

    Update main.js

--- behavior_packs/scripts/main.js
+++ behavior_packs/scripts/main.js
@@ -2,15 +2,13 @@ import { system, world, BlockComponentRegistry } from "@minecraft/server";
 import "./pale_oak_growth_drops";
 import "./ore_regeneration";
 import {NetheriteCrafterComponent} from "./netherite_crafter_components";
+system.beforeEvents.worldInitialize.subscribe(async({ blockTypeRegistry }) => {
+    blockTypeRegistry.registerCustomComponent(
+        "convertedjava:netherite_crafter_logic",
+        new NetheriteCrafterComponent()
+    );
+    console.log("Scripting API first step initialized")
+});
 world.afterEvents.worldIntialize.subscribe(()=>{
     world.sendMessage("Scripting API fully initalized");
 })
-const CUSTOM_COMPONENT_ID = "convertedjava:netherite_crafter_script:crafter_component"
-system.beforeEvents.worldInitalize.subscribe(initEvent => {
-    try {
-        initEvent.blockComponentRegistry.registerCustomComponent(CUSTOM_COMPONENT_ID, new NetheriteCrafterComponent())
-        world.sendMessage("Scripting API first step initialized.");
-    } catch (e) {
-        world.sendMessage("Scripting API first step unsuccessful. Continuing...");
-    }
-}
